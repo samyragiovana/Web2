dd4f205e2e2b50e0ca9d13fb4142174685980712
SAMYRA GIOVANA CARVALHO CAMPOS <0064125@IFMG.EDU.BR>
2024-04-09 16:14:11 -0300

    09-04

--- 08-04-back-node/app.mjs
+++ 08-04-back-node/app.mjs
@@ -10,8 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.get('/veiculos', veiculoController.all);
+app.get('/veiculos/:id', veiculoController.one);
 app.post('/veiculos', veiculoController.new);
 app.put('/veiculos', veiculoController.edit);
+app.delete('/veiculos', veiculoController.delete);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- 08-04-back-node/controllers/veiculoController.mjs
+++ 08-04-back-node/controllers/veiculoController.mjs
@@ -2,24 +2,39 @@
 import Veiculo from "../Models/veiculo.mjs";
 
 const veiculoController = {
-    "new":async(req,res) => {
-        const created = await Veiculo.create ({
-            fabricante: req.body.fabricante, 
-            modelo: req.body.modelo 
-          });
-          res.send(created);
+    "new": async (req, res) => {
+        const created = await Veiculo.create({
+            fabricante: req.body.fabricante,
+            modelo: req.body.modelo
+        });
+        res.send(created);
     },
-    "all":async(req,res) => {
-        res.json(await Veiculo.findAll() );
+    "one": async (req, res) => {
+        const v = await Veiculo.findOne({
+            where: { id: req.params.id }
+        })
+        res.json(v);
     },
 
-    "edit":async(req,res) => {
+    "all": async (req, res) => {
+        res.json(await Veiculo.findAll());
+    },
+
+    "edit": async (req, res) => {
         const v = await Veiculo.findOne({
-            where: {id: req.body.id}
+            where: { id: req.body.id }
         });
         v.fabricante = req.body.fabricante;
         v.modelo = req.body.modelo;
         await v.save();
+        res.json(v);
+
+    },
+    "delete": async (req, res) => {
+        const v = await Veiculo.findOne({
+            where: { id: req.body.id }
+        });
+        await v.destroy();
         res.json(v);
 
     }
